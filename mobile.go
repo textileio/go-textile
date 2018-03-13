7d623d9655ae8d2d33cb8d1204eeba3b4ba7d48b
Sander Pick <sanderpick@gmail.com>
2018-03-12 17:13:45 -0700

    use a flag for repo dir in mobile main (for debug)

--- mobile.go
+++ mobile.go
@@ -1,8 +1,21 @@
 package main
 
-import "github.com/textileio/textile-go/mobile"
+import (
+	"github.com/jessevdk/go-flags"
+	"github.com/textileio/textile-go/mobile"
+	"os"
+)
+
+var opts struct {
+	RepoDir string `short:"d" long:"dir" description:"Rep directory" required:"true"`
+}
 
 func main()  {
-	textile := mobile.NewTextile("/Users/sander/go/src/github.com/textileio/textile-go/.ipfs")
+	_, err := flags.Parse(&opts)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	textile := mobile.NewTextile(opts.RepoDir)
 	textile.Start()
 }
