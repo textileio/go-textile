f02a418fbaa4be78f81050484c41c372c7b3f15a
Aaron Sutula <aaron@textile.io>
2018-03-15 08:47:16 -0600

    Add a static library target

---
+++ react-native-textile-ipfs/ios/TextileIFPSx/TextileIFPSx.h
@@ -0,0 +1,13 @@
+//
+//  TextileIFPSx.h
+//  TextileIFPSx
+//
+//  Created by Aaron Sutula on 3/15/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TextileIFPSx : NSObject
+
+@end
---
+++ react-native-textile-ipfs/ios/TextileIFPSx/TextileIFPSx.m
@@ -0,0 +1,13 @@
+//
+//  TextileIFPSx.m
+//  TextileIFPSx
+//
+//  Created by Aaron Sutula on 3/15/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import "TextileIFPSx.h"
+
+@implementation TextileIFPSx
+
+@end
