141f7aaa04c2770f119df73d4f14ba4f21704a36
Sander Pick <sanderpick@gmail.com>
2018-03-15 19:40:32 -0700

    add pin service endpoint

---
+++ net/util.go
@@ -0,0 +1,18 @@
+package net
+
+import (
+	"net/http"
+	"encoding/json"
+)
+
+func GetJson(client *http.Client, url string, target interface{}) error {
+	r, err := client.Get(url)
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
+
+	return json.NewDecoder(r.Body).Decode(target)
+}
+
+
