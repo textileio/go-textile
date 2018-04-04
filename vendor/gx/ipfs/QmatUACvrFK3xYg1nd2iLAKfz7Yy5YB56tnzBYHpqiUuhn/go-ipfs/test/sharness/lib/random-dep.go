05181867cd0c66c144c55076a015e0a0d0d7dc0f
Sander Pick <sanderpick@gmail.com>
2018-04-04 08:50:51 -0700

    build(vendor): ignore vendor

--- vendor/gx/ipfs/QmatUACvrFK3xYg1nd2iLAKfz7Yy5YB56tnzBYHpqiUuhn/go-ipfs/test/sharness/lib/random-dep.go
+++
@@ -1,9 +0,0 @@
-// package randomdep is here to introduce a dependency in random for godep to
-// function properly. this way we can keep go-random vendored and not
-// accidentally break our tests when we change it.
-package randomdep
-
-import (
-	_ "gx/ipfs/QmatUACvrFK3xYg1nd2iLAKfz7Yy5YB56tnzBYHpqiUuhn/go-ipfs/Godeps/_workspace/src/github.com/jbenet/go-random"
-	_ "gx/ipfs/QmatUACvrFK3xYg1nd2iLAKfz7Yy5YB56tnzBYHpqiUuhn/go-ipfs/Godeps/_workspace/src/github.com/jbenet/go-random-files"
-)
