d55163dd10d6d05260effe51fbdf12b174217519
andrew <andrewxhill@gmail.com>
2018-04-07 14:44:02 -0700

    Adds working confirmation code embeded in qr code for testing with app

--- desktop/message.go
+++ desktop/message.go
@@ -7,26 +7,40 @@ import (
 	qrcode "github.com/skip2/go-qrcode"
 	"github.com/asticode/go-astilog"
 	"encoding/base64"
+	"fmt"
+	"math/rand"
 )
 // Init exploration
 type IpfsResponse struct {
 	path string `json:"path"`
 	data string `json:"data"`
 }
+type QRCodeResponse struct {
+	png string `json:"png"`
+	code string `json:"code"`
+}
 // handleMessages handles messages
 func handleMessages(_ *astilectron.Window, m bootstrap.MessageIn) (payload interface{}, err error) {
 	switch m.Name {
 	case "peer.qr":
 
+		// create a random confirmation code
+		code := fmt.Sprintf("%04d", rand.Int63n(1e4))
+
 		var png []byte
 		// I've registered this URL so that apple will do an App Link from any url like it directly into our
 		// app. Just need to do a PR in the app to receive it
-		png, err := qrcode.Encode("https://www.textile.io/threads/pair=testing", qrcode.Medium, 256)
+
+		png, err := qrcode.Encode(fmt.Sprintf("https://www.textile.io/clients?code=%s", code), qrcode.Medium, 256)
 		if err != nil {
 			astilog.Errorf("qr generation failed: %s", err)
-			return err.Error(), err
+			return nil, err
 		}
-		payload = base64.StdEncoding.EncodeToString(png)
+		res := map[string]interface{}{
+			"png": string(base64.StdEncoding.EncodeToString(png)),
+			"code": code,
+		}
+		return res, nil
 
 	case "ipfs.getPath":
 		// Unmarshal payload
