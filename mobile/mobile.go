e73b7764aab77f574b870079639a7d05d11a9c5f
Sander Pick <sanderpick@gmail.com>
2018-04-09 13:10:34 -0700

    feat(desktop): pair with desktop method
    the pair with desktop method publishes the mobile client's recovery phrase, encrypted with the given
    pub key

--- mobile/mobile.go
+++ mobile/mobile.go
@@ -8,6 +8,9 @@ import (
 
 	tcore "github.com/textileio/textile-go/core"
 	"github.com/textileio/textile-go/net"
+
+	"gx/ipfs/QmZoWKhxUmZ2seW4BzX6fJkNR8hh9PsGModr7q171yq2SS/go-libp2p-peer"
+	libp2p "gx/ipfs/QmaPbCnUMBohSGo3KnxEa2bHqyJVVeEEcwtqJAYxerieBo/go-libp2p-crypto"
 )
 
 type Wrapper struct {
@@ -84,4 +87,43 @@ func (w *Wrapper) GetFileBase64(path string) (string, error) {
 // provides the user's recovery phrase for their private key
 func (w *Wrapper) GetRecoveryPhrase() (string, error) {
 	return w.node.Datastore.Config().GetMnemonic()
+}
+
+func (w *Wrapper) PairDesktop(pkb64 string) (string, error) {
+	pkb, err := base64.StdEncoding.DecodeString(pkb64)
+	if err != nil {
+		return "", err
+	}
+
+	pk, err := libp2p.UnmarshalPublicKey(pkb)
+	if err != nil {
+		return "", err
+	}
+
+	// the phrase will be used by the desktop client to create
+	// the private key needed to decrypt photos
+	ph, err := w.GetRecoveryPhrase()
+	if err != nil {
+		return "", err
+	}
+	// encypt with the desktop's pub key
+	cph, err := net.Encrypt(pk, []byte(ph))
+	if err != nil {
+		return "", err
+	}
+
+	// get the topic to pair with from the pub key
+	peerID, err := peer.IDFromPublicKey(pk)
+	if err != nil {
+		return "", err
+	}
+	topic := peerID.Pretty()
+
+	// finally, publish the encrypted phrase
+	err = w.node.IpfsNode.Floodsub.Publish(topic, cph)
+	if err != nil {
+		return "", err
+	}
+
+	return topic, nil
 }
