adf9f7e8b0e16e4fb9669c9821ca4f9ae5613a5a
andrew <andrewxhill@gmail.com>
2018-04-09 11:21:24 -0700

    feat(Adds method for grabbing the node's public key and embedding that key in the QR code):

--- desktop/message.go
+++ desktop/message.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"encoding/json"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"math/rand"
 
 	"github.com/asticode/go-astilectron"
 	"github.com/asticode/go-astilectron-bootstrap"
-	"github.com/skip2/go-qrcode"
 	"github.com/asticode/go-astilog"
+	"github.com/skip2/go-qrcode"
 )
 
 // Init exploration
@@ -18,9 +18,10 @@ type IpfsResponse struct {
 	data []byte `json:"data"`
 }
 type QRCodeResponse struct {
-	png string `json:"png"`
+	png  string `json:"png"`
 	code string `json:"code"`
 }
+
 // handleMessages handles messages
 func handleMessages(_ *astilectron.Window, m bootstrap.MessageIn) (payload interface{}, err error) {
 	switch m.Name {
@@ -29,18 +30,26 @@ func handleMessages(_ *astilectron.Window, m bootstrap.MessageIn) (payload inter
 		// create a random confirmation code
 		code := fmt.Sprintf("%04d", rand.Int63n(1e4))
 
+		pubKey, err := textile.GetPublicKey()
+		if err != nil {
+			astilog.Errorf("public key generation failed: %s", err)
+			return nil, err
+		}
+		pubKeyS := string(base64.StdEncoding.EncodeToString(pubKey))
+
 		var png []byte
 		// I've registered this URL so that apple will do an App Link from any url like it directly into our
 		// app. Just need to do a PR in the app to receive it
-
-		png, err := qrcode.Encode(fmt.Sprintf("https://www.textile.io/clients?code=%s", code), qrcode.Medium, 256)
+		png, err = qrcode.Encode(fmt.Sprintf("https://www.textile.io/clients?code=%s&key=%s", code, pubKeyS), qrcode.Medium, 256)
 		if err != nil {
 			astilog.Errorf("qr generation failed: %s", err)
 			return nil, err
 		}
 		res := map[string]interface{}{
-			"png": string(base64.StdEncoding.EncodeToString(png)),
+			"png":  string(base64.StdEncoding.EncodeToString(png)),
 			"code": code,
+			"url":  fmt.Sprintf("https://www.textile.io/clients?code=%s&key=%s", code, pubKeyS),
+			"key":  pubKeyS,
 		}
 		return res, nil
 
