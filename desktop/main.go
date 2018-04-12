f1fe60fe8c165f9c8c8d786c933cb6c970cbfb79
Sander Pick <sanderpick@gmail.com>
2018-04-11 17:56:37 -0700

    feat(desktop): finish pairing flow
    refactor pairing flow, use isotope for the minimal photo grid layout

--- desktop/main.go
+++ desktop/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"flag"
-	"fmt"
+
 	"github.com/asticode/go-astilectron"
 	"github.com/asticode/go-astilectron-bootstrap"
 	"github.com/asticode/go-astilog"
@@ -27,26 +27,26 @@ func main() {
 	// Create a desktop textile node
 	// TODO: on darwin, I think repo should live in Application Support
 	var err error
-	textile, _ = core.NewNode("output/.ipfs", false)
+	textile, err = core.NewNode("output/.ipfs", false)
+	if err != nil {
+		astilog.Errorf("create desktop node failed: %s", err)
+		return
+	}
 
 	// Bring the node online
 	err = textile.Start()
 	if err != nil {
-		astilog.Errorf("start mobile node failed: %s", err)
+		astilog.Errorf("start desktop node failed: %s", err)
+		return
 	}
 
 	// Start garbage collection and gateway services
-	// NOTE: on desktop, gateway runs on 8081
-	var errc = make(chan error)
+	// NOTE: on desktop, gateway runs on 8182, decrypting file gateway on 9192
+	var servErrc = make(chan error)
 	go func() {
-		errc <- textile.StartServices()
-		close(errc)
+		servErrc <- textile.StartServices()
+		close(servErrc)
 	}()
-
-	pairId, _ := textile.Datastore.Config().GetPairedID()
-	//if err != nil {
-	//	astilog.Errorf("get paired id failed: %s", err)
-	//}
 
 	// Run bootstrap
 	astilog.Debugf("Running app built at %s", BuiltAt)
@@ -60,64 +60,13 @@ func main() {
 		Debug:          *debug,
 		Homepage:       "index.html",
 		MessageHandler: handleMessages,
-		OnWait: func(_ *astilectron.Astilectron, iw *astilectron.Window, _ *astilectron.Menu, _ *astilectron.Tray, _ *astilectron.Menu) error {
-			//w = iw
-			if pairId != "" {
-				// tmp: if paired id is present, start listening
-				var errc3 = make(chan error)
-				go func() {
-					errc3 <- textile.StartSync(pairId)
-					close(errc3)
-				}()
-
-				iw.SendMessage(map[string]string{"name": "ready", "gateway": "http://localhost:9192"})
-
-				iw.SendMessage(map[string]string{
-					"name":      "sync.new",
-					"hash":      "QmbZPvPb9kmsRJDC5Zi8Jw5M6stT7J6i7L8FkPJYbAfE3F",
-					"timestamp": "today",
-				})
-				// TODO:
-				// Each time a new hash is delivered, issue
-				// iw.SendMessage(map[string]string{"name": "sync.new", "hash": hash})
-
-				// Grab all the current hashes in the datastore and populate the gallery
-				for _, photo := range textile.Datastore.Photos().GetPhotos("-1", 10) {
-					fmt.Printf("Photo: %s", photo.Cid)
-					iw.SendMessage(map[string]string{
-						"name":      "sync.new",
-						"hash":      photo.Cid,
-						"timestamp": photo.Timestamp.String(),
-					})
-				}
-			} else {
-				// tmp: sub to own peer id for pairing setup
-				// this should really only happen when you click "pair"
-				// and then be closed
-				var errc2 = make(chan error)
-				go func() {
-					errc2 <- textile.StartPairing()
-					// TODO: When this completes we should send the onboard.complete message
-					// iw.SendMessage(map[string]string{"name": "onboard.complete"})
-					close(errc2)
-				}()
-				/*
-					If the desktop client hasn't already paired with a mobile device, tell the UI to init onboarding
-				*/
-				iw.SendMessage(map[string]string{"name": "onboard"})
-
-				// TODO:
-				// TODO: When StartPairing is complete, we should issue the message
-				// iw.SendMessage(map[string]string{"name": "pairing.complete"})
-			}
-			return nil
-		},
-		RestoreAssets: RestoreAssets,
+		OnWait:         start,
+		RestoreAssets:  RestoreAssets,
 		WindowOptions: &astilectron.WindowOptions{
-			BackgroundColor: astilectron.PtrStr("#333"),
+			BackgroundColor: astilectron.PtrStr("#333333"),
 			Center:          astilectron.PtrBool(true),
-			Height:          astilectron.PtrInt(700),
-			Width:           astilectron.PtrInt(700),
+			Height:          astilectron.PtrInt(800),
+			Width:           astilectron.PtrInt(1280),
 		},
 	}); err != nil {
 		astilog.Fatal(errors.Wrap(err, "running bootstrap failed"))
---
+++ desktop/pairing.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/asticode/go-astilectron"
+	"github.com/asticode/go-astilog"
+)
+
+func start(_ *astilectron.Astilectron, iw *astilectron.Window, _ *astilectron.Menu, _ *astilectron.Tray, _ *astilectron.Menu) error {
+	astilog.Info("TEXTILE STARTED")
+
+	// check for an existing paired mobile id
+	pairedID, err := textile.Datastore.Config().GetPairedID()
+	if err != nil {
+		return err
+	}
+	if pairedID != "" {
+		// if we have one, start syncing
+		astilog.Info("FOUND MOBILE PEER ID")
+
+		// tell app what peer id we're gonna sync with
+		sendData(iw, "sync.ready", map[string]interface{}{
+			"pairedID": pairedID,
+			"html":     getPhotosHTML(),
+		})
+
+	} else {
+		// otherwise, start onboaring
+		astilog.Info("NO MOBILE PEER ID FOUND")
+		astilog.Info("STARTING PAIRING")
+
+		// sub to own peer id for pairing setup
+		go func() {
+			var idc = make(chan string)
+			var errc = make(chan error)
+			go textile.StartPairing(idc, errc)
+			select {
+			case id := <-idc:
+				if id == "" {
+					astilog.Errorf("failed to parse new paired id: %s", err)
+					return
+				}
+
+				// let the app know we're done pairing
+				sendMessage(iw, "onboard.complete")
+
+				// and that we're ready to go
+				sendData(iw, "sync.ready", map[string]interface{}{
+					"pairedID": id,
+					"html":     getPhotosHTML(),
+				})
+			case err := <-errc:
+				astilog.Errorf("error while pairing: %s", err)
+			}
+		}()
+		sendMessage(iw, "onboard.start")
+	}
+
+	return nil
+}
+
+func startSyncing(iw *astilectron.Window, pairedID string) {
+	astilog.Info("STARTING SYNC")
+
+	// start subscription
+	var errc = make(chan error)
+	var datac = make(chan string)
+	go func() {
+		errc <- textile.StartSync(pairedID, datac)
+		close(errc)
+	}()
+
+	for {
+		select {
+		case hash := <-datac:
+			sendData(iw, "sync.data", map[string]interface{}{
+				"hash": hash,
+			})
+		case err := <-errc:
+			astilog.Errorf("error while syncing: %s", err)
+		}
+	}
+}
+
+func getPhotosHTML() string {
+	var html string
+	for _, photo := range textile.Datastore.Photos().GetPhotos("", -1) {
+		html += fmt.Sprintf(
+			"<div class=\"grid-item\"><img src=\"http://localhost:9192/ipfs/%s/thumb\" /></div>",
+			photo.Cid,
+		)
+	}
+	return html
+}
+
+func sendMessage(iw *astilectron.Window, name string) {
+	iw.SendMessage(map[string]string{"name": name})
+}
+
+func sendData(iw *astilectron.Window, name string, data map[string]interface{}) {
+	data["name"] = name
+	iw.SendMessage(data)
+}
