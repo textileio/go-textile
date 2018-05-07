4048938ea9806df90cdad5af74a99f3dedccb414
Sander Pick <sanderpick@gmail.com>
2018-05-07 15:39:37 -0700

    feat(core): use Ed25519 for peer identity, capture ipfs logging
    fixes #77

---
+++ util/logging.go
@@ -0,0 +1,38 @@
+package util
+
+import (
+	"bufio"
+	"os"
+
+	"github.com/op/go-logging"
+)
+
+type StdOutLogger struct {
+	log    *logging.Logger
+	stdout *os.File
+	r      *os.File
+	w      *os.File
+}
+
+func NewStdOutLogger(log *logging.Logger) (*StdOutLogger, error) {
+	r, w, err := os.Pipe()
+	if err != nil {
+		return nil, err
+	}
+	return &StdOutLogger{log: log, stdout: os.Stdout, r: r, w: w}, nil
+}
+
+func (s *StdOutLogger) Start() {
+	os.Stdout = s.w
+	go func() {
+		scanner := bufio.NewScanner(s.r)
+		for scanner.Scan() {
+			s.log.Info(scanner.Text())
+		}
+	}()
+}
+
+func (s *StdOutLogger) Stop() {
+	s.w.Close()
+	os.Stdout = s.stdout
+}
