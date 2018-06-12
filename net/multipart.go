99695180026477e889d06a207e40427d5d1356f8
Sander Pick <sanderpick@gmail.com>
2018-06-12 12:28:38 -0700

    feat(mobile): post multipart share payload to central

--- net/multipart.go
+++ net/multipart.go
@@ -2,6 +2,8 @@ package net
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"path/filepath"
 )
@@ -20,11 +22,11 @@ func (m *MultipartRequest) Init(dir string, boundary string) {
 
 func (m *MultipartRequest) AddFile(b []byte, fname string) error {
 	// create file if not yet exists
-	f, err := os.OpenFile(m.PayloadPath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	file, err := os.OpenFile(m.PayloadPath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
 	if err != nil {
 		return err
 	}
-	defer f.Close()
+	defer file.Close()
 
 	// create boundary header
 	header := fmt.Sprintf("--%s", m.Boundary)
@@ -36,13 +38,13 @@ func (m *MultipartRequest) AddFile(b []byte, fname string) error {
 	header += nl
 
 	// write the header and data part
-	if _, err = f.Write([]byte(header)); err != nil {
+	if _, err = file.Write([]byte(header)); err != nil {
 		return err
 	}
-	if _, err = f.Write(b); err != nil {
+	if _, err = file.Write(b); err != nil {
 		return err
 	}
-	if _, err = f.Write([]byte(nl)); err != nil {
+	if _, err = file.Write([]byte(nl)); err != nil {
 		return err
 	}
 
@@ -51,20 +53,47 @@ func (m *MultipartRequest) AddFile(b []byte, fname string) error {
 
 func (m *MultipartRequest) Finish() error {
 	// open file, will error if not first inited
-	f, err := os.OpenFile(m.PayloadPath, os.O_APPEND|os.O_WRONLY, 0600)
+	file, err := os.OpenFile(m.PayloadPath, os.O_APPEND|os.O_WRONLY, 0600)
 	if err != nil {
 		return err
 	}
-	defer f.Close()
+	defer file.Close()
 
 	// create boundary footer
 	footer := fmt.Sprintf("--%s--", m.Boundary)
 	footer += nl
 
 	// write the footer
-	if _, err = f.Write([]byte(footer)); err != nil {
+	if _, err = file.Write([]byte(footer)); err != nil {
 		return err
 	}
+
+	return nil
+}
+
+func (m *MultipartRequest) Send(url string) error {
+	// open file, will error if not first inited
+	file, err := os.Open(m.PayloadPath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// include boundary in header
+	req, err := http.NewRequest("POST", url, file)
+	req.Header.Set("Content-Type", fmt.Sprintf("multipart/form-data; boundary=%s", m.Boundary))
+
+	// make the request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("response Status:", resp.Status)
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("response Body:", string(body))
 
 	return nil
 }
