a1d9de8e806777c0d743c773f591c37eb01ef502
Sander Pick <sanderpick@gmail.com>
2018-06-12 18:06:49 -0700

    fix(crypto): remove rsa

--- crypto/asymmetric.go
+++ crypto/asymmetric.go
@@ -1,41 +1,13 @@
 package crypto
 
 import (
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/hmac"
 	"crypto/rand"
-	"crypto/sha256"
-	"encoding/binary"
 	"errors"
-	"golang.org/x/crypto/hkdf"
 	"golang.org/x/crypto/nacl/box"
 	libp2p "gx/ipfs/QmaPbCnUMBohSGo3KnxEa2bHqyJVVeEEcwtqJAYxerieBo/go-libp2p-crypto"
-	"io"
 )
 
 const (
-	// The version of the encryption algorithm used. Currently only 1 is supported
-	CiphertextVersion = 1
-
-	// Length of the serialized version in bytes
-	CiphertextVersionBytes = 4
-
-	// Length of the secret key used to generate the AES and MAC keys in bytes
-	SecretKeyBytes = 32
-
-	// Length of the AES key in bytes
-	AESKeyBytes = 32
-
-	// Length of the MAC key in bytes
-	MacKeyBytes = 32
-
-	// Length of the RSA encrypted secret key ciphertext in bytes
-	EncryptedSecretKeyBytes = 512
-
-	// Length of the MAC in bytes
-	MacBytes = 32
-
 	// Length of nacl nonce
 	NonceBytes = 24
 
@@ -44,24 +16,11 @@ const (
 )
 
 var (
-	// The ciphertext cannot be shorter than CiphertextVersionBytes + EncryptedSecretKeyBytes + aes.BlockSize + MacKeyBytes
-	ErrShortCiphertext = errors.New("ciphertext is too short")
-
-	// The HMAC included in the ciphertext is invalid
-	ErrInvalidHmac = errors.New("invalid Hmac")
-
 	// Nacl box decryption failed
 	BoxDecryptionError = errors.New("failed to decrypt curve25519")
-
-	// Satic salt used in the hdkf
-	Salt = []byte("Textile Encryption Algorithm")
 )
 
 func Encrypt(pubKey libp2p.PubKey, bytes []byte) ([]byte, error) {
-	rsaPubkey, ok := pubKey.(*libp2p.RsaPublicKey)
-	if ok {
-		return encryptRSA(rsaPubkey, bytes)
-	}
 	ed25519Pubkey, ok := pubKey.(*libp2p.Ed25519PublicKey)
 	if ok {
 		return encryptCurve25519(ed25519Pubkey, bytes)
@@ -102,72 +61,7 @@ func encryptCurve25519(pubKey *libp2p.Ed25519PublicKey, bytes []byte) ([]byte, e
 	return ciphertext, nil
 }
 
-func encryptRSA(pubKey *libp2p.RsaPublicKey, bytes []byte) ([]byte, error) {
-	// Encrypt random secret key with RSA pubkey
-	secretKey := make([]byte, SecretKeyBytes)
-	rand.Read(secretKey)
-
-	encKey, err := pubKey.Encrypt(secretKey)
-	if err != nil {
-		return nil, err
-	}
-
-	// Derive MAC and AES keys from the secret key using hkdf
-	hash := sha256.New
-
-	hkdfr := hkdf.New(hash, secretKey, Salt, nil)
-
-	aesKey := make([]byte, AESKeyBytes)
-	_, err = io.ReadFull(hkdfr, aesKey)
-	if err != nil {
-		return nil, err
-	}
-	macKey := make([]byte, MacKeyBytes)
-	_, err = io.ReadFull(hkdfr, macKey)
-	if err != nil {
-		return nil, err
-	}
-
-	// Encrypt message with the AES key
-	block, err := aes.NewCipher(aesKey)
-	if err != nil {
-		return nil, err
-	}
-
-	/* The IV needs to be unique, but not secure. Therefore it is common to
-	   include it at the beginning of the ciphertext. */
-	ciphertext := make([]byte, aes.BlockSize+len(bytes))
-	iv := ciphertext[:aes.BlockSize]
-	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-		return nil, err
-	}
-
-	stream := cipher.NewCFBEncrypter(block, iv)
-	stream.XORKeyStream(ciphertext[aes.BlockSize:], bytes)
-
-	// Create the HMAC
-	mac := hmac.New(sha256.New, macKey)
-	mac.Write(ciphertext)
-	messageMac := mac.Sum(nil)
-
-	// Prepend the ciphertext with the encrypted secret key
-	ciphertext = append(encKey, ciphertext...)
-
-	// Prepend version
-	version := make([]byte, CiphertextVersionBytes)
-	binary.BigEndian.PutUint32(version, uint32(CiphertextVersion))
-	ciphertext = append(version, ciphertext...)
-
-	// Append the MAC
-	ciphertext = append(ciphertext, messageMac...)
-	return ciphertext, nil
-}
-
 func Decrypt(privKey libp2p.PrivKey, ciphertext []byte) ([]byte, error) {
-	rsaPrivkey, ok := privKey.(*libp2p.RsaPrivateKey)
-	if ok {
-		return decryptRSA(rsaPrivkey, ciphertext)
-	}
 	ed25519Privkey, ok := privKey.(*libp2p.Ed25519PrivateKey)
 	if ok {
 		return decryptCurve25519(ed25519Privkey, ciphertext)
@@ -198,63 +92,4 @@ func decryptCurve25519(privKey *libp2p.Ed25519PrivateKey, ciphertext []byte) ([]
 		return nil, BoxDecryptionError
 	}
 	return plaintext, nil
-}
-
-func decryptRSA(privKey *libp2p.RsaPrivateKey, ciphertext []byte) ([]byte, error) {
-	if len(ciphertext) < CiphertextVersionBytes+EncryptedSecretKeyBytes+aes.BlockSize+MacKeyBytes {
-		return nil, ErrShortCiphertext
-	}
-
-	// Decrypt the secret key using the RSA private key
-	secretKey, err := privKey.Decrypt(ciphertext[CiphertextVersionBytes : CiphertextVersionBytes+EncryptedSecretKeyBytes])
-	if err != nil {
-		return nil, err
-	}
-
-	// Derive the AES and MAC keys from the secret key using hdkf
-	hash := sha256.New
-
-	hkdfr := hkdf.New(hash, secretKey, Salt, nil)
-
-	aesKey := make([]byte, AESKeyBytes)
-	_, err = io.ReadFull(hkdfr, aesKey)
-	if err != nil {
-		return nil, err
-	}
-	macKey := make([]byte, MacKeyBytes)
-	_, err = io.ReadFull(hkdfr, macKey)
-	if err != nil {
-		return nil, err
-	}
-
-	// Calculate the HMAC and verify it is correct
-	mac := hmac.New(sha256.New, macKey)
-	mac.Write(ciphertext[CiphertextVersionBytes+EncryptedSecretKeyBytes : len(ciphertext)-MacBytes])
-	messageMac := mac.Sum(nil)
-	if !hmac.Equal(messageMac, ciphertext[len(ciphertext)-MacBytes:]) {
-		return nil, ErrInvalidHmac
-	}
-
-	// Decrypt the AES ciphertext
-	block, err := aes.NewCipher(aesKey)
-	if err != nil {
-		return nil, err
-	}
-	ciphertext = ciphertext[CiphertextVersionBytes+EncryptedSecretKeyBytes : len(ciphertext)-MacBytes]
-	if len(ciphertext) < aes.BlockSize {
-		return nil, err
-	}
-	iv := ciphertext[:aes.BlockSize]
-	ciphertext = ciphertext[aes.BlockSize:]
-
-	stream := cipher.NewCFBDecrypter(block, iv)
-
-	// XORKeyStream can work in-place if the two arguments are the same
-	stream.XORKeyStream(ciphertext, ciphertext)
-	plaintext := ciphertext
-	return plaintext, nil
-}
-
-func getCipherTextVersion(ciphertext []byte) uint32 {
-	return binary.BigEndian.Uint32(ciphertext[:CiphertextVersionBytes])
 }
---
+++ net/multipart_test.go
@@ -0,0 +1,21 @@
+package net_test
+
+import (
+	"testing"
+)
+
+func TestMultipartRequest_Init(t *testing.T) {
+	// TODO
+}
+
+func TestMultipartRequest_AddFile(t *testing.T) {
+	// TODO
+}
+
+func TestMultipartRequest_Finish(t *testing.T) {
+	// TODO
+}
+
+func TestMultipartRequest_Send(t *testing.T) {
+	// TODO
+}
