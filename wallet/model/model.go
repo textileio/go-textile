1b95e8b6c615b0558551ffc8e15d88be5503fc91
Sander Pick <sanderpick@gmail.com>
2018-07-18 15:03:48 -0700

    refactor(photos): use full keys in json meta data

--- wallet/model/model.go
+++ wallet/model/model.go
@@ -12,23 +12,23 @@ type Profile struct {
 const ThumbnailWidth = 300
 
 type Metadata struct {
-	Username string    `json:"un,omitempty"`
-	Created  time.Time `json:"cts,omitempty"`
-	Added    time.Time `json:"ats,omitempty"`
+	Username string    `json:"username,omitempty"`
+	Created  time.Time `json:"created,omitempty"`
+	Added    time.Time `json:"added"`
 }
 
 type FileMetadata struct {
 	Metadata
-	Name string `json:"name,omitempty"`
-	Ext  string `json:"ext,omitempty"`
+	Name string `json:"name"`
+	Ext  string `json:"extension"`
 }
 
 type PhotoMetadata struct {
 	FileMetadata
-	Format          string  `json:"fmt"`
-	ThumbnailFormat string  `json:"fmttn"`
+	Format          string  `json:"format"`
+	ThumbnailFormat string  `json:"format_thumb"`
 	Width           int     `json:"width"`
 	Height          int     `json:"height"`
-	Latitude        float64 `json:"lat,omitempty"`
-	Longitude       float64 `json:"lon,omitempty"`
+	Latitude        float64 `json:"latitude,omitempty"`
+	Longitude       float64 `json:"longitude,omitempty"`
 }
