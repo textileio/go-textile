ec7ec2d07a0ba49de3f692e2f255660e8daccb9d
Sander Pick <sanderpick@gmail.com>
2018-07-27 13:19:38 -0700

    refactor(joins): joiner should not be echoed to

--- cafe/pin.go
+++ cafe/pin.go
@@ -23,6 +23,7 @@ func (c *Cafe) pin(g *gin.Context) {
 		// unpack archive
 		gr, err := gzip.NewReader(g.Request.Body)
 		if err != nil {
+			log.Errorf("error creating gzip reader %s", err)
 			g.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
@@ -33,15 +34,18 @@ func (c *Cafe) pin(g *gin.Context) {
 				break
 			}
 			if err != nil {
+				log.Errorf("error getting tar next %s", err)
 				g.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 				return
 			}
 			switch header.Typeflag {
 			case tar.TypeDir:
+				log.Error("got nested directory, aborting")
 				g.JSON(http.StatusBadRequest, gin.H{"error": "directories are not supported"})
 				return
 			case tar.TypeReg:
 				if err := util.AddFileToDirectory(c.Ipfs(), dirb, tr, header.Name); err != nil {
+					log.Errorf("error adding file to dir %s", err)
 					g.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 					return
 				}
@@ -53,23 +57,32 @@ func (c *Cafe) pin(g *gin.Context) {
 		// pin the directory
 		dir, err := dirb.GetNode()
 		if err != nil {
+			log.Errorf("error creating dir node %s", err)
 			g.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
 		}
 		if err := util.PinDirectory(c.Ipfs(), dir, []string{}); err != nil {
+			log.Errorf("error pinning dir node %s", err)
 			g.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
 		}
 		id = dir.Cid()
 
-	default:
+	case "application/octet-stream":
 		var err error
 		id, err = util.PinData(c.Ipfs(), g.Request.Body)
 		if err != nil {
+			log.Errorf("error pinning raw body %s", err)
 			g.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
 		}
+	default:
+		log.Errorf("got bad content type %s", cType)
+		g.JSON(http.StatusBadRequest, gin.H{"error": "invalid content-type"})
+		return
 	}
+
+	log.Debugf("pinned request with content type %s: %s", cType, id)
 
 	// ship it
 	g.JSON(http.StatusCreated, gin.H{
