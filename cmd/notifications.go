8dbba164ab794f5b43e431bc6dab692b68a5f957
Sander Pick <sanderpick@gmail.com>
2018-08-15 21:44:21 -0700

    feat(cmd): add notifications commands

---
+++ cmd/notifications.go
@@ -0,0 +1,54 @@
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"github.com/fatih/color"
+	"github.com/textileio/textile-go/core"
+	"gopkg.in/abiosoft/ishell.v2"
+)
+
+func ListNotifications(c *ishell.Context) {
+	notifs := core.Node.Wallet.GetNotifications("", -1, "")
+	unread := core.Node.Wallet.CountUnreadNotifications()
+	if len(notifs) == 0 {
+		c.Println("no notifications found")
+	} else {
+		c.Println(fmt.Sprintf("found %d notifications, %d unread", len(notifs), unread))
+	}
+
+	yellow := color.New(color.FgHiYellow).SprintFunc()
+	for _, notif := range notifs {
+		body := notif.Body
+		if !notif.Read {
+			body += " (unread)"
+		}
+		c.Println(yellow(fmt.Sprintf("%s: %s", notif.Id, body)))
+	}
+}
+
+func ReadNotification(c *ishell.Context) {
+	if len(c.Args) == 0 {
+		c.Err(errors.New("missing notification id"))
+		return
+	}
+	id := c.Args[0]
+
+	if err := core.Node.Wallet.ReadNotification(id); err != nil {
+		c.Err(err)
+		return
+	}
+
+	yellow := color.New(color.FgHiYellow).SprintFunc()
+	c.Println(yellow("ok, marked as read"))
+}
+
+func ReadAllNotifications(c *ishell.Context) {
+	if err := core.Node.Wallet.ReadAllNotifications(); err != nil {
+		c.Err(err)
+		return
+	}
+
+	yellow := color.New(color.FgHiYellow).SprintFunc()
+	c.Println(yellow("ok, marked all as read"))
+}
--- textile.go
+++ textile.go
@@ -458,6 +458,29 @@ func main() {
 			})
 			shell.AddCmd(deviceCmd)
 		}
+		{
+			notificationCmd := &ishell.Cmd{
+				Name:     "notification",
+				Help:     "manage notifications",
+				LongHelp: "List and read notifications.",
+			}
+			notificationCmd.AddCmd(&ishell.Cmd{
+				Name: "read",
+				Help: "mark a notification as read",
+				Func: cmd.ReadNotification,
+			})
+			notificationCmd.AddCmd(&ishell.Cmd{
+				Name: "readall",
+				Help: "mark all notifications as read",
+				Func: cmd.ReadAllNotifications,
+			})
+			notificationCmd.AddCmd(&ishell.Cmd{
+				Name: "ls",
+				Help: "list notifications",
+				Func: cmd.ListNotifications,
+			})
+			shell.AddCmd(notificationCmd)
+		}
 
 		shell.Run()
 	}
@@ -496,6 +519,20 @@ func start() error {
 				case wallet.DeviceRemoved:
 					break
 				}
+			}
+		}
+	}()
+
+	// subscribe to notifications
+	go func() {
+		yellow := color.New(color.FgHiYellow).SprintFunc()
+		for {
+			select {
+			case notification, ok := <-core.Node.Wallet.Notifications():
+				if !ok {
+					return
+				}
+				fmt.Println(yellow(notification.Body))
 			}
 		}
 	}()
