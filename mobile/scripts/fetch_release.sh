f5cbc35e4accefcf745581af638f3ef61cd74321
Sander Pick <sanderpick@gmail.com>
2018-09-07 10:19:51 -0700

    feat(go-mobile): add mobile npm package

---
+++ mobile/scripts/fetch_release.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DIR="$(dirname "$(stat -f "$0")")"
+IOS_DIR="${DIR}/../ios"
+ANDROID_DIR="${DIR}/../android"
+
+# fetch iOS framework
+mkdir -p ${IOS_DIR}
+wget https://github.com/textileio/textile-go/releases/download/v0.1.4/textile-go_v0.1.4_ios-framework.tar.gz
+tar xvfz textile-go_v0.1.4_ios-framework.tar.gz
+rm textile-go_v0.1.4_ios-framework.tar.gz
+mv Mobile.framework ${IOS_DIR}
+
+# fetch Android framework
+mkdir -p ${ANDROID_DIR}
+wget https://github.com/textileio/textile-go/releases/download/v0.1.4/textile-go_v0.1.4_android_aar.tar.gz
+tar xvfz textile-go_v0.1.4_android_aar.tar.gz
+rm textile-go_v0.1.4_android_aar.tar.gz
+mv textilego.aar ${ANDROID_DIR}
