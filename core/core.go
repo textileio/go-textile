1f5d0d26119af15eb26c78e8d57bb2f8a138cb54
Sander Pick <sanderpick@gmail.com>
2018-09-11 08:40:05 -0700

    chore(release): bump v0.1.7

--- core/core.go
+++ core/core.go
@@ -17,7 +17,7 @@ var fileLogFormat = logging.MustStringFormatter(
 )
 var log = logging.MustGetLogger("core")
 
-const Version = "0.1.5"
+const Version = "0.1.7"
 
 // Node is the single TextileNode instance
 var Node *TextileNode
