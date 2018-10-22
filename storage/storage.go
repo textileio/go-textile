4961c9f7a89324c452f8a9ad396a2d69a07a48cf
Sander Pick <sanderpick@gmail.com>
2018-10-22 08:22:55 -0700

    refactor(storage): remove old storage module

--- storage/storage.go
+++
@@ -1,9 +0,0 @@
-package storage
-
-import (
-	ma "gx/ipfs/QmYmsdtJ3HsodkePE3eU3TsCaP2YvPZJ4LoXnNkDE5Tpt7/go-multiaddr"
-)
-
-type OfflineMessagingStorage interface {
-	Store(message []byte) (ma.Multiaddr, error)
-}
