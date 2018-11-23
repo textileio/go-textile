0662f6a33c114199fe693cfcc627bf364dd94f85
Sander Pick <sanderpick@gmail.com>
2018-11-22 19:36:26 -0800

    test(mills): fix json mill test

--- mill/json_test.go
+++ mill/json_test.go
@@ -5,61 +5,11 @@ import (
 )
 
 func TestJson_Mill(t *testing.T) {
-	m := &Json{
-		Opts: JsonOpts{
-			Schema: `
-{
-  "$id": "https://example.com/person.schema.json",
-  "$schema": "http://json-schema.org/draft-07/schema#",
-  "title": "Person",
-  "type": "object",
-  "properties": {
-    "firstName": {
-      "type": "string",
-      "description": "The person's first name."
-    },
-    "lastName": {
-      "type": "string",
-      "description": "The person's last name."
-    },
-    "age": {
-      "description": "Age in years which must be equal to or greater than zero.",
-      "type": "integer",
-      "minimum": 0
-    }
-  },
-  "required": [
-    "age"
-  ],
-  "additionalProperties": false
-}
-`,
-		},
-	}
+	m := &Json{}
 
-	doc1 := `
-{"firstName": "Grigori", "lastName": "Rasputin"}
-`
-	doc2 := `
-{"firstName": "Grigori", "lastName": "Rasputin", "age": "foo"}
-`
-	doc3 := `
-{"firstName": "Grigori", "lastName": "Rasputin", "age": 47}
-`
-	doc4 := `
-{"firstName": "Grigori", "lastName": "Rasputin", "age": 47, "whacko": true}
-`
+	obj := `{"firstName": "Grigori", "lastName": "Rasputin", "age": 47}`
 
-	if _, err := m.Mill([]byte(doc1), "test"); err == nil {
-		t.Error("age should be required")
-	}
-	if _, err := m.Mill([]byte(doc2), "test"); err == nil {
-		t.Error("age should be int type")
-	}
-	if _, err := m.Mill([]byte(doc3), "test"); err != nil {
-		t.Error(err)
-	}
-	if _, err := m.Mill([]byte(doc4), "test"); err == nil {
-		t.Error("should not allow extra props")
+	if _, err := m.Mill([]byte(obj), "test"); err != nil {
+		t.Fatal(err)
 	}
 }
