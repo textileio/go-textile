1c0d33000dc049449a21b94e1f1788e3c54c59da
Sander Pick <sanderpick@gmail.com>
2019-01-08 12:20:40 -0800

    config: add textile bootstrap peers

--- repo/config/init_ipfs.go
+++ repo/config/init_ipfs.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"time"
 
 	native "gx/ipfs/QmPEpj17FDRpc7K1aArKZp3RsHtzRMKykeK9GVgn4WQGPR/go-ipfs-config"
@@ -28,13 +29,39 @@ var DefaultServerFilters = []string{
 	"/ip4/240.0.0.0/ipcidr/4",
 }
 
+// TextileBootstrapAddresses are the addresses of cafe nodes run by the Textile team.
+var TextileBootstrapAddresses = []string{
+	"/ip4/18.144.12.135/tcp/4001/ipfs/12D3KooWGBW3LfzypK3zgV4QxdPyUm3aEuwBDMKRRpCPm9FrJvar",
+	"/ip4/13.57.23.210/tcp/4001/ipfs/12D3KooWQue2dSRqnZTVvikoxorZQ5Qyyug3hV65rYnWYpYsNMRE",
+	"/ip4/18.221.167.133/tcp/4001/ipfs/12D3KooWERmHT6g4YkrPBTmhfDLjfi8b662vFCfvBXqzcdkPGQn1",
+	"/ip4/18.224.173.65/tcp/4001/ipfs/12D3KooWLh9Gd4C3knv4XqCyCuaNddfEoSLXgekVJzRyC5vsjv5d",
+	"/ip4/35.180.16.103/tcp/4001/ipfs/12D3KooWDhSfXZCBVAK6SNQu7h6mfGCBJtjMS44PW5YA5YCjVmjB",
+	"/ip4/35.180.35.45/tcp/4001/ipfs/12D3KooWBCZEDkZ2VxdNYKLLUACWbXMvW9SpVbbvoFR9CtH4qJv9",
+	"/ip4/13.250.53.27/tcp/4001/ipfs/12D3KooWQ5MR9Ugz9HkVU3fYFbiWbQR4jxKJB66JoSY7nP5ShsqQ",
+	"/ip4/3.1.49.130/tcp/4001/ipfs/12D3KooWDWJ473M3fXMEcajbaGtqgr6i6SvDdh5Ru9i5ZzoJ9Qy8",
+}
+
+// TextileBootstrapPeers returns the (parsed) set of Textile bootstrap peers.
+func TextileBootstrapPeers() ([]native.BootstrapPeer, error) {
+	ps, err := native.ParseBootstrapPeers(TextileBootstrapAddresses)
+	if err != nil {
+		return nil, fmt.Errorf(`failed to parse hardcoded bootstrap peers: %s
+This is a problem with the Textile codebase. Please report it to the dev team.`, err)
+	}
+	return ps, nil
+}
+
+// InitIpfs create the IPFS config file
 func InitIpfs(identity native.Identity) (*native.Config, error) {
-	bootstrapPeers, err := native.DefaultBootstrapPeers()
+	ipfsPeers, err := native.DefaultBootstrapPeers()
 	if err != nil {
 		return nil, err
 	}
-
-	datastore := defaultDatastoreConfig()
+	textilePeers, err := TextileBootstrapPeers()
+	if err != nil {
+		return nil, err
+	}
+	peers := append(textilePeers, ipfsPeers...)
 
 	conf := &native.Config{
 		API: native.API{
@@ -47,8 +74,8 @@ func InitIpfs(identity native.Identity) (*native.Config, error) {
 		// NOTE: two swarm listen addrs, one tcp, one utp.
 		Addresses: addressesConfig(),
 
-		Datastore: datastore,
-		Bootstrap: native.BootstrapPeerStrings(bootstrapPeers),
+		Datastore: defaultDatastoreConfig(),
+		Bootstrap: native.BootstrapPeerStrings(peers),
 		Identity:  identity,
 		Discovery: native.Discovery{
 			MDNS: native.MDNS{
@@ -107,6 +134,7 @@ func InitIpfs(identity native.Identity) (*native.Config, error) {
 	return conf, nil
 }
 
+// UpdateIpfs updates the IPFS config file
 func UpdateIpfs(rep repo.Repo, key string, value interface{}) error {
 	if err := rep.SetConfigKey(key, value); err != nil {
 		log.Errorf("error setting %s: %s", key, err)
