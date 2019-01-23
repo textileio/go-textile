40872f351a5fc51ac55c20dc4046651d1f38ba29
Aaron Sutula <aaron.sutula@gmail.com>
2019-01-23 10:08:42 -0600

    mobile: Update other CafeSession mehods to return []byte

--- mobile/cafe.go
+++ mobile/cafe.go
@@ -21,50 +21,58 @@ func (m *Mobile) RegisterCafe(host string) error {
 // CafeSessions calls core CafeSessions
 func (m *Mobile) CafeSessions() ([]byte, error) {
 	if !m.node.Started() {
-		return make([]byte, 0), core.ErrStopped
+		return nil, core.ErrStopped
 	}
 
 	sessions, err := m.node.CafeSessions()
 	if err != nil {
-		return make([]byte, 0), err
+		return nil, err
 	}
 	cafeSessions := &pb.CafeSessions{
 		Values: sessions,
 	}
 	bytes, err := proto.Marshal(cafeSessions)
 	if err != nil {
-		return make([]byte, 0), err
+		return nil, err
 	}
 	return bytes, nil
 }
 
 // CafeSession calls core CafeSession
-func (m *Mobile) CafeSession(peerId string) (string, error) {
+func (m *Mobile) CafeSession(peerId string) ([]byte, error) {
 	if !m.node.Started() {
-		return "", core.ErrStopped
+		return nil, core.ErrStopped
 	}
 
 	session, err := m.node.CafeSession(peerId)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	if session == nil {
-		return "", nil
+		return nil, nil
 	}
-	return toJSON(session)
+	bytes, err := proto.Marshal(session)
+	if err != nil {
+		return nil, err
+	}
+	return bytes, nil
 }
 
 // RefreshCafeSession calls core RefreshCafeSession
-func (m *Mobile) RefreshCafeSession(peerId string) (string, error) {
+func (m *Mobile) RefreshCafeSession(peerId string) ([]byte, error) {
 	if !m.node.Started() {
-		return "", core.ErrOffline
+		return nil, core.ErrStopped
 	}
 
 	session, err := m.node.RefreshCafeSession(peerId)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	return toJSON(session)
+	bytes, err := proto.Marshal(session)
+	if err != nil {
+		return nil, err
+	}
+	return bytes, nil
 }
 
 // DeegisterCafe calls core DeregisterCafe
