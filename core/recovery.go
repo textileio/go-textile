58220c59aecbb2b55e15877f8108855b9f30edad
Sander Pick <sanderpick@gmail.com>
2019-02-11 16:13:03 -0800

    recovery: apply thread backups

--- core/recovery.go
+++ core/recovery.go
@@ -1,11 +1,17 @@
 package core
 
 import (
+	mh "gx/ipfs/QmPnFwZ2JXKnXgMw8CdBPxn7FWh6LLdjUjxV1fKHuJnkr8/go-multihash"
+	libp2pc "gx/ipfs/QmPvyPwuCgJ7pDmrKDxRtsScJgBaM5h4EpRL2qQJsmXf4n/go-libp2p-crypto"
+	peer "gx/ipfs/QmTRhk7cgjUf2gfQ3p2M9KPECNZEW9XUrmHcFCgog4cPgB/go-libp2p-peer"
+
 	"github.com/golang/protobuf/proto"
 	"github.com/golang/protobuf/ptypes"
 	"github.com/golang/protobuf/ptypes/any"
+	"github.com/segmentio/ksuid"
 	"github.com/textileio/textile-go/broadcast"
 	"github.com/textileio/textile-go/pb"
+	"github.com/textileio/textile-go/repo"
 )
 
 // FindThreadBackups searches the network for backups
@@ -72,12 +78,65 @@ func (t *Textile) FindThreadBackups(query *pb.ThreadBackupQuery, options *pb.Que
 	return tresCh, terrCh, cancel, nil
 }
 
-//// ApplyThreadBackup dencrypts and adds a thread from a backup
-//func (t *Textile) ApplyThreadBackup(backup *pb.CafeClientThread) error {
-//	plaintext, err := t.account.Decrypt(backup.Ciphertext)
-//	if err != nil {
-//		return err
-//	}
-//
-//
-//}
+// ApplyThreadBackup dencrypts and adds a thread from a backup
+func (t *Textile) ApplyThreadBackup(backup *pb.CafeThread) error {
+	// check if we're allowed to get an invite
+	// Note: just using a dummy thread here because having these access+sharing
+	// methods on Thread is very nice elsewhere.
+	dummy := &Thread{
+		initiator: backup.Initiator,
+		ttype:     repo.ThreadType(backup.Type),
+		sharing:   repo.ThreadSharing(backup.Sharing),
+		members:   backup.Members,
+	}
+	if !dummy.shareable(t.config.Account.Address, t.config.Account.Address) {
+		return ErrNotShareable
+	}
+
+	sk, err := libp2pc.UnmarshalPrivateKey(backup.Sk)
+	if err != nil {
+		return err
+	}
+
+	id, err := peer.IDFromPrivateKey(sk)
+	if err != nil {
+		return err
+	}
+	if thrd := t.Thread(id.Pretty()); thrd != nil {
+		// thread exists, aborting
+		return nil
+	}
+
+	var sch mh.Multihash
+	if backup.Schema != "" {
+		sch, err = mh.FromB58String(backup.Schema)
+		if err != nil {
+			return err
+		}
+
+	}
+	config := AddThreadConfig{
+		Key:       ksuid.New().String(),
+		Name:      backup.Name,
+		Schema:    sch,
+		Initiator: backup.Initiator,
+		Type:      repo.ThreadType(backup.Type),
+		Sharing:   repo.ThreadSharing(backup.Sharing),
+		Members:   backup.Members,
+		Join:      false,
+	}
+	thrd, err := t.AddThread(sk, config)
+	if err != nil {
+		return err
+	}
+
+	if err := thrd.followParents([]string{backup.Head}); err != nil {
+		return err
+	}
+	hash, err := mh.FromB58String(backup.Head)
+	if err != nil {
+		return err
+	}
+
+	return thrd.updateHead(hash)
+}
