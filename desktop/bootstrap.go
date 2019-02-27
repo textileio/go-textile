02d96d839c89afb4d4496a1ef626a0a7a8f3eca6
Carson Farmer <carson.farmer@gmail.com>
2019-02-26 23:29:17 -0800

    feature(desktop): clear out existing, simplify to tray app only

--- desktop/bootstrap.go
+++ desktop/bootstrap.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"errors"
-
 	"github.com/asticode/go-astilectron"
 	"github.com/asticode/go-astilectron-bootstrap"
 	"github.com/asticode/go-astilog"
+	"github.com/pkg/errors"
 )
 
 // bootstrapApp runs bootstrap. Moved to own file so we don't have to see Asset and RestoreAsset highlighed as errors :)
 func bootstrapApp() {
-	astilog.Debugf("Running app built at %s", builtAt)
 	if err := bootstrap.Run(bootstrap.Options{
 		Asset:    Asset,
 		AssetDir: AssetDir,
@@ -22,18 +20,15 @@ func bootstrapApp() {
 		Debug:         *debug,
 		OnWait:        start,
 		RestoreAssets: RestoreAssets,
-		Windows: []*bootstrap.Window{{
-			Homepage:       "index.html",
-			MessageHandler: handleMessage,
-			Options: &astilectron.WindowOptions{
-				Center:          astilectron.PtrBool(true),
-				Height:          astilectron.PtrInt(SetupSize),
-				Width:           astilectron.PtrInt(SetupSize),
-				BackgroundColor: astilectron.PtrStr("#ffffff"),
-				TitleBarStyle:   astilectron.TitleBarStyleHiddenInset,
-				Show:            astilectron.PtrBool(false),
+		TrayOptions:   &astilectron.TrayOptions{
+			Image:   astilectron.PtrStr("resources/tray.png"),
+			Tooltip: astilectron.PtrStr("Textile"),
+		},
+		TrayMenuOptions: []*astilectron.MenuItemOptions{
+			{
+				Type: astilectron.MenuItemTypeSeparator,
 			},
-		}},
+		},
 	}); err != nil {
 		astilog.Fatal(errors.Wrap(err, "bootstrap failed"))
 	}
