0223de5f1a76b27f2b1dee6652b8eaa48a1ae12d
Sander Pick <sanderpick@gmail.com>
2019-04-10 12:27:06 -0700

    mobile: remove started requirement on logs api

--- mobile/logs.go
+++ mobile/logs.go
@@ -2,16 +2,11 @@ package mobile
 
 import (
 	"github.com/golang/protobuf/proto"
-	"github.com/textileio/go-textile/core"
 	"github.com/textileio/go-textile/pb"
 )
 
 // SetLogLevel calls core SetLogLevel
 func (m *Mobile) SetLogLevel(level []byte) error {
-	if !m.node.Started() {
-		return core.ErrStopped
-	}
-
 	mlevel := new(pb.LogLevel)
 	if err := proto.Unmarshal(level, mlevel); err != nil {
 		return err
