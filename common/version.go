3e0ecce5ae59c1bbd2b7f4fea37b8e11e14c61a9
Sander Pick <sanderpick@gmail.com>
2019-06-04 18:29:17 -0700

    version: bump 0.2.3 [ci skip]
    Signed-off-by: Sander Pick <sanderpick@gmail.com>

--- common/version.go
+++ common/version.go
@@ -4,4 +4,4 @@ package common
 var GitCommit, GitBranch, GitState, GitSummary, BuildDate string
 
 // Version is the current application's version literal
-const Version = "0.2.2"
+const Version = "0.2.3"
