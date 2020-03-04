d87e7a4d79d6c7729ed19ba3fc852f25af8ffe77
Sander Pick <sanderpick@gmail.com>
2020-03-03 18:58:31 -0800

    gateway: remove path suffix handling (#957)
    Signed-off-by: Sander Pick <sanderpick@gmail.com>

--- common/version.go
+++ common/version.go
@@ -4,4 +4,4 @@ package common
 var GitCommit, GitBranch, GitState, GitSummary, BuildDate string
 
 // Version is the current application's version literal
-const Version = "0.7.6"
+const Version = "0.7.8"
